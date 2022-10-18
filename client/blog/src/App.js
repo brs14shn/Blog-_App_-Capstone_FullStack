648946f18c6d9630baddbe06a739610282dd1b3a
brs14shn <brsshn292929@gmail.com>
2022-10-18 23:38:42 +0300

    router && login && register && navbar created

--- client/blog/src/App.js
+++ client/blog/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import AppRouter from "./router/AppRouter"
+
 
 function App() {
   return (
-    <div className="App">
-     <h1>Blog Project </h1>
-    </div>
+    <>
+      <AppRouter/>
+    </>
   );
 }
 
