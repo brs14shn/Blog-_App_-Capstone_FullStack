1c42e01728a1523bcc2f5aa1fefacbd070bd48c9
brs14shn <brsshn292929@gmail.com>
2022-10-22 09:22:07 +0300

    admin && model finished

--- server/blog/models.py
+++ server/blog/models.py
@@ -27,32 +27,39 @@ class Post(models.Model):
     title = models.CharField(max_length=100)    
     content = models.TextField()
     image = models.URLField(default='https://picsum.photos/500/300?random=1',blank=True)
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, blank=True, null=True,related_name="categorys")
     publish_date = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.CharField(max_length=10, choices=OPTIONS, default='d')
 
+    def __str__(self):
+        return f"{self.title}-{self.author}"
+
      # @property
     # def quiz_count(self):
     #     return self.quizz.count()  # related name varsa
         #return self.quiz_set.count() # related name yoksa classname.lower()_set
-    def comment_count(self):
-        return self.comment_set.all().count()
-    
-    def view_count(self):
-        return self.postview_set.all().count()
-    
-    def like_count(self):
-        return self.like_set.all().count()
-    
-    def comments(self):
-        return self.comment_set.all()
 
+    # def comment_count(self):
+    #     return self.comment_set.all().count()
+        
+    # def comment_count(self):
+    #     return self.comment_post.all().count()
+    
+    # def view_count(self):
+    #     return self.postview_set.all().count()
+    
+    # def like_count(self):
+    #     return self.like_set.all().count()
+    
+    # def comments(self):
+    #     return self.comment_set.all()
+
+    
 class Comment(TimeStamp):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comment_user", default="Anonymous User")
     post = models.ForeignKey(Post, on_delete=models.CASCADE,related_name="comment_post")
-
     content = models.TextField()
     
     def __str__(self):
